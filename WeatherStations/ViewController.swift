cad273ec606080ded3967a3db501bcce24e0d431
yyeung <yvonnemyeung@gmail.com>
2017-08-10 21:51:02 -0700

    Focus map on CA
    startMap() loads mapview and centers the screen over CA.

--- WeatherStations/ViewController.swift
+++ WeatherStations/ViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let regionRadius: CLLocationDistance = 1500000
+    
+    // Starts the Map view and focus it on Fresno, CA
+    func startMap() {
+        let initialLocation = CLLocation(latitude: 36.746841, longitude: -119.772591)
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(initialLocation.coordinate, regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        startMap()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
